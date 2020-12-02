5a388d30f249af5d80d61214ce75fcf2a91e4947
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-12-01 23:30:54 -0300

    upload files

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../assets/logo.svg'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <img src={logo} className="home-logo"/>
+                <h3>Aguarde, estamos chegando...</h3>
+                <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Home
