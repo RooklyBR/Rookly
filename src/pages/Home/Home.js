a593a0b74d10c12e544cc91eedee1755dd2f6284
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-12-02 00:52:33 -0300

    fixed logo svg

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ import './Home.css'
 const Home = () => {
     return (
         <div className="home-container">
-            <img src={logo} className="home-logo"/>
+            <img src={logo} className="home-logo" alt="rookly logo"/>
                 <h3>Aguarde, estamos chegando...</h3>
                 <div className="loader"></div>
         </div>
